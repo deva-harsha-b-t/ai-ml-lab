288b36b91004334f8ed2b25075087e8ae00eb7d5
Prajwal-R <prajwalbhat30@gmail.com>
2022-01-04 09:41:04 +0530

    updated ann

--- ANN(backpropogation)/backpropogation.py
+++ ANN(backpropogation)/backpropogation.py
@@ -1,8 +1,8 @@
 import numpy as np
-X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
+X = np.array(([2, 9], [1, 5], [3, 6]))
+Y = np.array(([92], [86], [89]))
 X = X/np.amax(X,axis=0) # maximum of X array longitudinally
-y = y/100
+Y = Y/100
 #Sigmoid Function
 def sigmoid (x):
     return 1/(1 + np.exp(-x))
@@ -11,7 +11,7 @@ def derivatives_sigmoid(x):
     return x * (1 - x)
 #Variable initialization
 epoch=7000 #Setting training iterations
-lr=0.1 #Setting learning rate
+lr=0.5 #Setting learning rate
 inputlayer_neurons = 2 #number of features in data set
 hiddenlayer_neurons = 3 #number of hidden layers neurons
 output_neurons = 1 #number of neurons at output layer
@@ -24,24 +24,26 @@ bout=np.random.uniform(size=(1,output_neurons))
 
 for i in range(epoch):
 #Forward Propogation
-    hinp1=np.dot(X,wh)
+    hinp1=X.dot(wh)
     hinp=hinp1 + bh
     hlayer_act = sigmoid(hinp)
-    outinp1=np.dot(hlayer_act,wout)
+
+    outinp1=hlayer_act.dot(wout)
     outinp= outinp1+ bout
     output = sigmoid(outinp)
 #Backpropagation
-    EO = y-output
+    EO = Y-output
     outgrad = derivatives_sigmoid(output)
     d_output = EO* outgrad
+    
     EH = d_output.dot(wout.T)
     hiddengrad = derivatives_sigmoid(hlayer_act)#how much hidden layer wts contributed to error
     d_hiddenlayer = EH * hiddengrad
+    
     wout += hlayer_act.T.dot(d_output) *lr# dotproduct of nextlayererror and currentlayerop
-   # bout += np.sum(d_output, axis=0,keepdims=True) *lr
+    
     wh += X.T.dot(d_hiddenlayer) *lr
-    #bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
-print("Input: \n" + str(X)) 
-print("Actual Output: \n" + str(y))
-print("Predicted Output: \n" ,output)
 
+print("Input: \n" + str(X)) 
+print("Actual Output: \n" + str(Y))
+print("Predicted Output: \n" ,output)
