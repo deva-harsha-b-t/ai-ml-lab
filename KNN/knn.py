e06015bc5d3fa3d8df41f161cad9e2f980934379
Prajwal-R <prajwalbhat30@gmail.com>
2022-01-11 21:48:15 +0530

    updated

--- KNN/knn.py
+++ KNN/knn.py
@@ -6,8 +6,8 @@ from sklearn import datasets
 iris=datasets.load_iris()
 iris_data=iris.data
 iris_labels=iris.target
-print(iris_data)
-print(iris_labels)
+#print(iris_data)
+#print(iris_labels)
 x_train, x_test, y_train, y_test=train_test_split(iris_data,iris_labels,test_size=0.30)
 
 classifier=KNeighborsClassifier(n_neighbors=5)
